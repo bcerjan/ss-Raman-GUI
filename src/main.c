3940e8953bc998c2a24a220d08cfb1885366562d
ben <bcerjan@gmail.com>
2020-12-14 21:10:58 -0600

    Updates to main.c -- testing including the wavepond library. Currently not working as expected (or at all...) if including calls to wavepond-API functions.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
+// GTK for GUI generation
 #include <gtk/gtk.h>
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// Header files for the waveform generator ("wavepond")
+#include "dax22000_lib_DLL64.h"
+
+// Variable to track if we're currently running a scan or not:
+static int scan_running = 0;
+
+// Both the "X" button and File->Quit should end the process
 void on_window_main_destroy()
 {
   gtk_main_quit();
@@ -12,16 +24,30 @@ void on_file_quit_activate()
   gtk_main_quit();
 }
 
+// Handlers for the various buttons:
+
+// Re-scan for spectrometers:
 void spectrometer_scan_clicked_cb()
 {
   g_print("Spectrometer_scan clicked\n");
 }
 
-void scan_button_clicked_cb()
+// We want to start or stop a real scan:
+void scan_button_clicked_cb(GtkButton *button)
 {
   g_print("scan_button clicked\n");
+  if (scan_running == 0) { // We are not currently running a scan
+    scan_running = 1;
+    char *text = "Stop Scan";
+    gtk_button_set_label(button, text);
+  } else {
+    scan_running = 0;
+    char *text = "Start Scan";
+    gtk_button_set_label(button, text);
+  }
 }
 
+// Someone wants to see the help documentation
 void on_help_help_activate()
 {
   g_print("Open help!\n");
@@ -38,7 +64,7 @@ int main(int    argc,
   GtkWidget  *window;
 
   // Load custom CSS:
-  GFile  *cssFile = g_file_new_for_path("css/test.css");
+  GFile  *cssFile = g_file_new_for_path("css/progressBar.css");
   GtkCssProvider *cssProvider = gtk_css_provider_new();
   gtk_css_provider_load_from_file(cssProvider, cssFile, NULL);
   gtk_style_context_add_provider_for_screen(gdk_screen_get_default(),
@@ -61,11 +87,18 @@ int main(int    argc,
   // Dereference builder as its job is done:
   g_object_unref(builder);
 
+  // Set up waveform generator:
+  DWORD NumCards = 0;
+  NumCards = DAx22000_GetNumCards();
+  printf("Number of Cards = %d\n", NumCards);
+
   // Show the main window:
   gtk_widget_show(window);
 
   // Run the main loop:
   gtk_main();
+
+  // TODO: Add turn-off of spectrometers / function generator here
 
   return 0;
 }
