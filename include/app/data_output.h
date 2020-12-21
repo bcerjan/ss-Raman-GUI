8927be3646f54657461664ca555ba32093c28611
ben <bcerjan@gmail.com>
2020-12-21 14:42:09 -0600

    Updated dataCheckboxes structure to use int's so we don't need to include gtk/glib everywhere

--- include/app/data_output.h
+++ include/app/data_output.h
@@ -3,12 +3,12 @@
 
 #include <gtk/gtk.h>
 
-// Note: check the state using if (dataCheckboxes->raw_data) {}
+// Note: check the state using: if (dataCheckboxes->raw_data) {}
 struct dataCheckboxes {
-  gboolean raw_data;
-  gboolean fft_data;
-  gboolean conv_data;
-  gboolean final_data;
+  int raw_data;
+  int fft_data;
+  int conv_data;
+  int final_data;
 };
 
 void output_data(char *data_dir, char *fname, struct dataCheckboxes *checkboxes);
