048068ea5f5475a0c20e9179e06dfd3aa50fa535
ben <bcerjan@gmail.com>
2021-01-05 14:39:47 -0600

    Updated header file to point to new asynchronous function and remove previous function definitions as they are no longer used

--- include/app/acquire_data.h
+++ include/app/acquire_data.h
@@ -6,16 +6,20 @@ struct dataAcqParams {
   int spectrometerIndex;
   int integrationTime; // in ms
   int measurement_reps; // in #
+  int mod_freq; // in Hz
   struct dataOuputOpts *outputPtr;
   GtkWidget *progressBar;
   int timeoutID;
   int timeoutInterval;
   GtkWidget *btn; // Start/Stop button
-  struct dataAcqParams *self; // So we can free our struct from a thread
+  //struct dataAcqParams *self; // So we can free our struct from a thread
+  GCancellable *cancellable;
 };
 
-void *start_data_acq(gpointer data);
+void start_data_acq_async(gpointer            data,
+                          GCancellable       *cancellable,
+                          GAsyncReadyCallback callback,
+                          gpointer            user_data);
 int progressBar_timeout_cb(gpointer data);
-void *stop_data_acq(gpointer data);
 
 #endif
