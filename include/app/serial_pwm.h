e4ddca811099c9142c41892037bb82622d4304cb
ben <bcerjan@gmail.com>
2021-03-11 13:18:27 -0600

    Updates to header file and source file for control of laser power via serial port. Now using libserialport from the sigrock project to increase portability.

--- include/app/serial_pwm.h
+++ include/app/serial_pwm.h
@@ -1,6 +1,8 @@
 #ifndef SERIAL_PWM
 #def SERIAL_PWM
 
+char **get_port_list(int *len);
+void free_port_list(char **list);
 void set_duty_cycle(gchar port_id, float percent);
 
 #endif
--- src/serial_pwm.c
+++ src/serial_pwm.c
@@ -2,58 +2,84 @@
 // laser power.
 
 #include <gtk/gtk.h>
+#include "libserialport.h"
 
-// How long could our URI potentially be (in characters)
-#define MAX_URI_LENGTH 60
+#include "measurement_params.h"
+
 #define SENDING_STR_LEN 15 // we use a float which has a maximum of 9, so leave some room
 
-void set_duty_cycle(gchar port_id, float percent)
+// Function to get list of strings identifying available ports
+// usage:
+// int length = 0;
+// char *port_names = get_port_list(&length); // needs to be freed by caller
+char ** get_port_list(int *len)
 {
-  // first, format port number into a URI in a platform-dependent way
-  const gchar portURI[MAX_URI_LENGTH] = generate_port_uri(port_id);
+  struct sp_port **port_list;
+  enum sp_return result = sp_list_ports(&port_list);
 
-  // Now we need to write to our port:
-  const gchar strToSend[SENDING_STR_LEN];
-  sprintf(strToSend, "<%f>", percent); // use < and > as start/end markers for transmission
+  if (result != SP_OK) {
+    printf("sp_list_ports() failed!\n");
+    return NULL;
+  }
 
-  // Open serial port:
-  GIOChannel *serialCon;
-  GError **error;
-  gsize *bytes_written;
+  // First count how many we found:
+  int i,j;
+  for (i = 0; port_list[i] != NULL; i++) {}
+  *len = i;
+  char **res;
+  res = g_malloc(i * sizeof(*res));
 
-  serialCon = g_io_channel_new_file(portURI, "w", error);
+  for (j = 0; j < i; j++) {
+    struct sp_port *port = port_list[j];
+    res[j] = g_strdup(sp_get_port_name(port));
+  }
 
-  g_io_channel_write_chars(serialCon, strToSend, -1, bytes_written, error);
+  sp_free_port_list(port_list);
 
-  g_io_channel_shutdown(serialCon, TRUE, error);
+  return res;
+}
+
+// Function to free list produced by the above function
+void free_port_list(int len, char **list)
+{
+  int i;
+  for (i = 0; i < len; i++) {
+    g_free(list[i]);
+  }
+  g_free(list);
 
   return;
 
 }
 
-
-// function to generate a string containing URI for the serial port:
-*gchar generate_port_uri(gchar port_id)
+// Function to actually set a particular duty cycle:
+void set_duty_cycle(gchar port_name, float percent)
 {
-  /*
-  port_id is something like: COM2 (Windows), ttyS0 (linux, potentially also
-  ttySUSB0 or ttyUSB0), or tty.usbserial-A700dYoR on OSX. On Windows, our return
-  should be like: "\\\\.\\COM2". On the *nix based ones, we need an output like
-  "/dev/ttyUSB0" (or whatever)
-  */
-  gchar output[MAX_URI_LENGTH];
+  // String to write to our port:
+  const gchar strToSend[SENDING_STR_LEN];
+  sprintf(strToSend, "<%f>", percent); // use < and > as start/end markers for transmission
 
-  #if defined _WIN32
-    output = g_strjoin(NULL, "\\\\\\\\.\\\\", port_id, NULL);
-    // we need 4 backslashes then 2 backslashes, but have to escape each one
-    // making this mess
-  #endif
+  // Open and configure the port:
+  struct sp_port *port;
+  sp_get_port_by_name(port_name, &port);
+  sp_open(port, SP_MODE_READ_WRITE);
 
-  #if defined(__unix__) || defined(__unix) || \
-        (defined(__APPLE__) && defined(__MACH__))
+  // Set our port parameters (defined in measurement_params.h):
+  sp_set_baudrate(port, SERIAL_BAUD_RATE);
+  sp_set_bits(port, SERIAL_BITS);
+  sp_set_parity(port, SERIAL_PARITY);
+  sp_set_stopbits(port, SERIAL_STOP_BITS);
+  sp_set_flowcontrol(port, SERIAL_FLOW_CONTROL);
 
-    output = g_strjoin(NULL, "/dev/", port_id, NULL);
-  #endif
+  int strLen = strlen(strToSend);
+  int timeout = 500; // 500 ms timeout
+  sp_blocking_write(port, strToSend, strLen, timeout);
 
-  return output;
+  // Read here to verify?
+
+  sp_close(port);
+  sp_free_port(port);
+
+  return;
+
 }
