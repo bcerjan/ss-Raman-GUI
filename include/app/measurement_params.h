3e8153671257b0469a6aed9435eae1459aa86531
ben <bcerjan@gmail.com>
2021-08-26 14:07:21 -0500

    Updated to includ a value for how much signal the waveform generator needs to output

--- include/app/measurement_params.h
+++ include/app/measurement_params.h
@@ -7,6 +7,12 @@
 #define PN_CODE_LENGTH_OPTS 6
 #define LASER_WAVELENGTH 638.318 // in nm
 
+// Parameters for electro-optic modulator
+#define WVFM_MAGNITUDE 850 // Dependent on your waveform generator and/or amplifier
+                           // Ours accepts values 0-4095, but with the amplifier
+                           // we need only a fraction of that (850 into 50 Ohms)
+
+
 static const int mod_freqs[MODULATION_OPTS] = {100, 250, 500}; // In MHz
 static const int pn_code_lengths[PN_CODE_LENGTH_OPTS] = {32, 64, 128, 256, 512, 1024};
 // If you change pn_code_lengths, you will also need to update pn_code_generator.c
